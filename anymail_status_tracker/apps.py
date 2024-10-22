0f5f99683c55aac37b22d94c22ec015623ef1887
Sebastian Braß <kontakt@sebastian-brass.de>
2024-10-22 16:46:35 +0200

    Create model to store mail tracking status.

---
+++ anymail_status_tracker/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+
+
+class SESSNSTrackerConfig(AppConfig):
+    name = "anymail_status_tracker"
+    verbose_name = "Anymail Status Tracker"
+    default_auto_field = "django.db.models.BigAutoField"
+
+    def ready(self):
+        from . import signals  # NOQA
