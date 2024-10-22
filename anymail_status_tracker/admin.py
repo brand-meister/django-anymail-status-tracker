27ca45d810b3637b7dc66c9aa76d8993fff6cb15
Sebastian Braß <kontakt@sebastian-brass.de>
2024-10-22 16:46:56 +0200

    Create admin site for MailDelivery model.

---
+++ anymail_status_tracker/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from anymail_status_tracker.models import MailDelivery
+
+
+@admin.register(MailDelivery)
+class MailDeliveryAdmin(admin.ModelAdmin):
+    list_display = ('recipient', 'sent_at', 'updated_at', 'state')
+    list_filter = ('state',)
+    readonly_fields = ('sent_at', 'updated_at')
+    ordering = ('-updated_at',)
