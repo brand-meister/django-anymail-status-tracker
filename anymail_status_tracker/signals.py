9d930bd30c671de7d8cfa38ed432d2fe77717821
Sebastian Braß <kontakt@sebastian-brass.de>
2025-10-23 09:46:22 +0200

    Fix missing recipient in MailDelivery objects.
    Re #2

--- anymail_status_tracker/signals.py
+++ anymail_status_tracker/signals.py
@@ -7,11 +7,12 @@ from anymail_status_tracker.models import MailDelivery
 
 @receiver(post_send)
 def handle_post_send(sender, message, status, esp_name, **kwargs):
-    for _, recipient_status in status.recipients.items():
+    for recipient, recipient_status in status.recipients.items():
         MailDelivery.objects.create(
             message_id=recipient_status.message_id,
             state=recipient_status.status,
             esp_name=esp_name,
+            recipient=recipient,
         )
 
 
@@ -21,7 +22,7 @@ def handle_tracking_event(sender, event, esp_name, **kwargs):
         esp_name=esp_name,
         state=event.event_type,
         timestamp=event.timestamp,
-        metadata=event.metadata,
+        metadata=event.metadata or {},
         reject_reason=event.reject_reason,
         description=event.description,
         mta_response=event.mta_response,
