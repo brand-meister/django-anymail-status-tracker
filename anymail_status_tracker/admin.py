590ba8103697a0e8eb8fecb11cd22cb56d59f894
Sebastian Braß <kontakt@sebastian-brass.de>
2025-10-23 09:46:38 +0200

    MailDelivery admin improvements.
    Re #2

--- anymail_status_tracker/admin.py
+++ anymail_status_tracker/admin.py
@@ -7,5 +7,7 @@ from anymail_status_tracker.models import MailDelivery
 class MailDeliveryAdmin(admin.ModelAdmin):
     list_display = ("recipient", "sent_at", "updated_at", "state")
     list_filter = ("state",)
+    search_fields = ("recipient",)
+    date_hierarchy = "sent_at"
     readonly_fields = ("sent_at", "updated_at")
     ordering = ("-updated_at",)
