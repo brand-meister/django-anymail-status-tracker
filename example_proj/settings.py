ee8cdcf3332d8c63bbc43ae12d7b03c33cc14185
Fabio Reinsch <fabio.reinsch@brandpunktmeister.de>
2026-03-19 22:58:26 +0100

    Update database configuration in settings.py to use BASE_DIR for database path
    - Modified the database name setting to construct the path using os.path.join(BASE_DIR, "db.sqlite3") for better portability and clarity.

--- example_proj/settings.py
+++ example_proj/settings.py
@@ -43,7 +43,7 @@ WSGI_APPLICATION = "example_proj.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": "db.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 EMAIL_BACKEND = "anymail.backends.console.EmailBackend"
