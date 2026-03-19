b936e9119f091bb3385e4ca43d29a66c7559faa3
Fabio Reinsch <fabio.reinsch@brandpunktmeister.de>
2026-03-19 22:58:19 +0100

    Refactor FORM_EVENT_TYPES in TestForm to use tuple for consistency
    - Changed FORM_EVENT_TYPES from a generator expression to a tuple for improved performance and clarity.

--- example_proj/forms.py
+++ example_proj/forms.py
@@ -4,6 +4,6 @@ from anymail_status_tracker.debug.data import EVENT_TYPES
 
 
 class TestForm(forms.Form):
-    FORM_EVENT_TYPES = ((x, x) for x in EVENT_TYPES)
+    FORM_EVENT_TYPES = tuple((x, x) for x in EVENT_TYPES)
 
     webhook_status_type = forms.ChoiceField(choices=FORM_EVENT_TYPES)
