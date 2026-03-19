266c344018cb06535d37afb7b0b21271db79e2c9
Fabio Reinsch <fabio.reinsch@brandpunktmeister.de>
2026-03-19 12:42:41 +0100

    added logging and better error handling
    Re #11

--- anymail_status_tracker/signals.py
+++ anymail_status_tracker/signals.py
@@ -1,11 +1,17 @@
+import logging
+
 from django.contrib.admin.models import CHANGE, LogEntry
 from django.contrib.contenttypes.models import ContentType
+from django.db import IntegrityError
 from django.dispatch import receiver
 
 from anymail.signals import post_send, tracking
 
 from anymail_status_tracker.models import MailDelivery
 from anymail_status_tracker.settings import ANYMAIL_STATUS_TRACKER_LOG_ACTION_USER_ID
+
+
+logger = logging.getLogger("anymail_status_tracker")
 
 
 @receiver(post_send)
@@ -24,6 +30,10 @@ def handle_tracking_event(sender, event, esp_name, **kwargs):
     try:
         delivery = MailDelivery.objects.get(message_id=event.message_id, recipient=event.recipient)
     except MailDelivery.DoesNotExist:
+        logger.error("No delivery found for message %s and recipient %s", event.message_id, event.recipient)
+        return
+    except MailDelivery.MultipleObjectsReturned:
+        logger.error("Multiple deliveries found for message %s and recipient %s", event.message_id, event.recipient)
         return
 
     previous_state = delivery.state
@@ -41,11 +51,18 @@ def handle_tracking_event(sender, event, esp_name, **kwargs):
     delivery.save()
 
     if ANYMAIL_STATUS_TRACKER_LOG_ACTION_USER_ID:
-        LogEntry.objects.create(
-            user_id=ANYMAIL_STATUS_TRACKER_LOG_ACTION_USER_ID,
-            content_type_id=ContentType.objects.get_for_model(delivery).pk,
-            object_id=delivery.pk,
-            object_repr=str(delivery),
-            action_flag=CHANGE,
-            change_message=f"Status updated from {previous_state} to {delivery.state}",
-        )
+        try:
+            LogEntry.objects.create(
+                user_id=ANYMAIL_STATUS_TRACKER_LOG_ACTION_USER_ID,
+                content_type_id=ContentType.objects.get_for_model(delivery).pk,
+                object_id=delivery.pk,
+                object_repr=str(delivery)[:200],
+                action_flag=CHANGE,
+                change_message=f"Status updated from {previous_state} to {delivery.state}",
+            )
+        except IntegrityError:
+            logger.error(
+                "Error creating log entry for delivery %s: "
+                "Wrongly configured ANYMAIL_STATUS_TRACKER_LOG_ACTION_USER_ID",
+                delivery.id,
+            )
